4f4f590a456bdc710a20773f0859d558fcb1bc5a
Altuhim Md. Shaad <144701936+ShaadSD@users.noreply.github.com>
2026-05-12 17:02:06 +0600

    production update

--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@p)6r_vs2s=m0p@=xmch!a2i6o#ur)n18()a=zh9fv0#sy^g*q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "school-management-project-n5r1.onrender.com",
+]
 
 AUTH_USER_MODEL = 'accounts.User'
 # Application definition
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
 
@@ -47,8 +49,9 @@ INSTALLED_APPS = [
     'results',
     'attendance'
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
